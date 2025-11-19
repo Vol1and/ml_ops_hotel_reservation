45cf2f78331a18d769a8bb3bc6d99de38a50c8cd
Полторацкий Владимир <vladimir.poltorackijj@vitech.team>
2025-11-19 17:34:42 -0500

    Update Jenkins & new Dockerfile

--- application.py
+++ application.py
@@ -40,7 +40,7 @@ def index():
     return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1337)
+    app.run(host='0.0.0.0', port=5000)
 
 
 
